aa98963d8159e7a77a80d9de74129c17a5bc4703
MaximeHoll <maxime.hollanders15@gmail.com>
2022-11-02 09:22:01 +0100

    Navbar ok, about ok, contacts ok responsive ok

--- resources/script/script.js
+++ resources/script/script.js
@@ -3,30 +3,24 @@
 
 ///////Nav menu
 
-let menuHamburger = document.querySelector(".navigation__burger");
-        const bars = document.querySelectorAll(".bar");
-        const navLinks = document.querySelector(".navigation__ul");
-        const logo = document.querySelector(".navigation__logo");
-        const line = document.querySelector(".linethrough");
+const menuHamburger = document.querySelector(".navigation__burger");
+const bars = document.querySelectorAll(".bar");
+const navLinks = document.querySelector(".navigation__ul");
 
-        console.log(line);
-        
+menuHamburger.addEventListener('click',()=>{
+    menuHamburger.classList.toggle("active");
+    bars[0].classList.toggle('nav-open');
+    bars[1].classList.toggle('nav-open');
+    bars[2].classList.toggle('nav-open');
+    navLinks.classList.toggle('sidebar');
+    })
 
-        menuHamburger.addEventListener('click',()=>{
-        menuHamburger.classList.toggle("active");
-        bars[0].classList.toggle('nav-open');
-        bars[1].classList.toggle('nav-open');
-        bars[2].classList.toggle('nav-open');
-        navLinks.classList.toggle('sidebar');
-        logo.classList.toggle('logo');
-        line.style.display="none";
-        })
+///Remove navbar when cliking a link inside of it, to be checked
 
-
-        document.querySelectorAll("li").forEach(n => n.addEventListener("click", () => {
-            menuHamburger.classList.remove("active");
-            navLinks.classList.remove('sidebar');
-            logo.classList.remove('logo');
-            line.style.display= "block";
-        }
-        ))
+        // document.querySelectorAll("li").forEach(n => n.addEventListener("click", () => {
+        //     menuHamburger.classList.remove("active");
+        //     navLinks.classList.remove('sidebar');
+        //     logo.classList.remove('logo');
+        //     line.style.display= "block";
+        // }
+        // ))
