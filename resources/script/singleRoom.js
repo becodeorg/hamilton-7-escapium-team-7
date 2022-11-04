ca72c1a8d03148b03e6fffd9796289838ba79ea5
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-11-04 14:15:16 +0100

    finish js single room

--- resources/script/singleRoom.js
+++ resources/script/singleRoom.js
@@ -7,8 +7,8 @@ async function init()
     var name = urlParams.get('room');
 
     let data = await loadJS(name);
-    let room = search(data, name);
-
+    let room = await search(data, name);
+    // console.log("room : "+room);
     updatePage(room);
 }
 
@@ -33,9 +33,53 @@ function search(data, room)
 {
     for (let i = 0; i < data.length; i++) {
         if (data[i].name == room) {
-            return generateRoom(data[i]);
+            return data[i];
         }
     }
+}
+
+function updatePage(room)
+{
+    //update h1
+    document.getElementsByTagName('h1')[0].innerHTML = room.name;
+    // console.log("ici: "+room.name);
+
+    let lockContainer = document.getElementById('dificult');
+    // lockContainer.innerHTML = "";
+    //create a lock
+    for (let i = 0; i < room.difficulty; i++) {//rooms.difficulty
+        let lock = document.createElement('i');
+        lock.className = 'fa fa-lock redColor';
+        lockContainer.appendChild(lock);
+    }
+    for (let i = 0; i < 5-room.difficulty; i++) {//rooms.difficulty
+        let lock = document.createElement('i');
+        lock.className = 'fa fa-lock whiteColor';
+        lockContainer.appendChild(lock);
+    }
+
+    //create data player
+    generate('fa fa-user','player',room.players);
+    //create data time
+    generate('fa fa-clock-o','minute',room.minutes);
+
+    generate('fa fa-phone','phone',room.phone);
+    generate('fa fa-envelope','mail',room.email);
+    generate('fa fa-skype','skype',room.skype);
+    generate('fa fa-map-marker','adress',room.location);
+
+}
+
+function generate(classe,id,data)
+{
+    let elemtime = document.getElementById(id);
+    // elemtime.innerHTML = "";
+    let ibalise = document.createElement('i');
+    ibalise.className = classe;//'fa fa-clock-o';
+    let text = document.createTextNode(data);//rooms.players;
+    
+    elemtime.appendChild(ibalise);
+    elemtime.appendChild(text);
 }
 
 init();
